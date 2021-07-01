25356edee081d89ad83ef3b7d5f828cc7b2611e2
Adrian Kuboszek <adrian.kuboszek@avanade.com>
2021-07-01 15:20:47 +0200

    Added mockup for last_update flag

--- app/solarman/pages/home.py
+++ app/solarman/pages/home.py
@@ -1,42 +1,43 @@
-from dash_bootstrap_components._components.Col import Col
-from dash_bootstrap_components._components.Row import Row
+import dash
 import dash_daq as daq
 import dash_bootstrap_components as dbc
 
+import dash_html_components as html
+
 from components import card
-import dash
 
 current_power = card(
     'current-power',
     '4.5kWh',
     'Current power'
-    )
+)
 
 avg_comparison = card(
     'avg',
     '+5%',
     'From average'
-    )
+)
 
 
 today_generation = card(
     'today-generation',
     '4.5kWh',
     'Today generation'
-    )
+)
 
 
 up_time = card(
     'uptime',
     '5:28h',
     'Uptime'
-    )
+)
 
 start_time = card(
     'start-time',
     '3:45',
     'System start'
-    )
+)
+
 
 layout = dbc.Col([
     dbc.Row([
@@ -49,10 +50,18 @@ layout = dbc.Col([
             dbc.Row([
                 dbc.Col(start_time),
                 dbc.Col(up_time),
+            ])
         ])
-    ])
-    ])
+    ]),
+    html.Div([
+        "Last update: 15:30"], style={
+        'text-align': 'center',
+        'width': '100%',
+        'position': 'fixed',
+        'bottom': 10,
+    })
 ])
+
 
 def init(server):
     app = dash.Dash(
@@ -64,10 +73,11 @@ def init(server):
     app.layout = layout
     return app.server
 
+
 if __name__ == '__main__':
     from flask import Flask
     app = Flask(__name__)
 
     with app.app_context():
         app = init(app)
-    app.run(debug=True)    
+    app.run(debug=True)
